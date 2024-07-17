6e483d069a951602ceb70b3290c58ef49601c5fc
Nahun92 <andrade_nahun@yahoo.com>
2024-07-17 15:04:43 -0400

    Human Resources Module

---
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/DashboardPage.java
@@ -0,0 +1,77 @@
+package com.chromatech.Cucumber_BDD_Testing.pages;
+
+import com.chromatech.utils.WebDriverUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage {
+
+    public DashboardPage() {
+        PageFactory.initElements(WebDriverUtils.driver, this);
+    }
+
+    /**
+     * Represents the Human Resource module WebElement on the login page.
+     */
+    @FindBy(xpath = "//span[normalize-space()='Human Resource']")
+    public WebElement humanResourceModule;
+
+    /**
+     * Represents the WebElement for the Staff Directory link on the Dashboard page.
+     */
+    @FindBy(xpath = "//ul[contains(@class,'treeview-menu menu-open')]//a[normalize-space()='Staff Directory']")
+    public WebElement staffDirectorySubModule;
+
+    /**
+     * Represents the WebElement for the Staff Attendance link on the Dashboard page.
+     */
+    @FindBy(xpath = "//ul[contains(@class,'treeview-menu menu-open')]//a[normalize-space()='Staff Attendance']")
+    public WebElement staffAttendanceSubModule;
+
+    /**
+     * Represents the WebElement for the Payroll link on the Dashboard page.
+     */
+    @FindBy(xpath = "//a[normalize-space()='Payroll']")
+    public WebElement payrollSubModule;
+
+    /**
+     * Represents the WebElement for the ApproveLeaveRequest link on Dashboard page.
+     */
+    @FindBy(xpath = "//a[normalize-space()='Approve Leave Request']")
+    public WebElement approveLeaveRequestSubModule;
+
+    /**
+     * Represents the WebElement for the Apply Leave link on the Dashboard page.
+     */
+    @FindBy(xpath = "//a[normalize-space()='Apply Leave']")
+    public WebElement applyLeaveSubModule;
+
+    /**
+     * Represents the WebElement for the Leave Type link on the Dashboard page.
+     */
+    @FindBy(xpath = "//a[normalize-space()='Leave Type']")
+    public WebElement leaveTypeSubModule;
+
+    /**
+     * Represents the WebElement for the Teachers Rating link on the Dashboard page.
+     */
+    @FindBy(xpath = "//a[normalize-space()='Teachers Rating']")
+    public WebElement teachersRatingSubModule;
+
+    /**
+     * Represents the Department link WebElement in the DashboardPage class.
+     */
+    @FindBy(xpath = "//a[normalize-space()='Department']")
+    public WebElement departmentSubModule;
+
+    /**
+     * Represents the Designation link WebElement in the DashboardPage class.
+     */
+    @FindBy(xpath = "//a[normalize-space()='Designation']")
+    public WebElement designationSubModule;
+
+
+}
+
+
--- src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Human_Resource_Module_Steps.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Human_Resource_Module_Steps.java
@@ -1,6 +1,6 @@
 package com.chromatech.Cucumber_BDD_Testing.stepDefinitions;
 
-import com.chromatech.Cucumber_BDD_Testing.pages.ExpenseModules;
+import com.chromatech.Cucumber_BDD_Testing.pages.DashboardPage;
 import com.chromatech.Cucumber_BDD_Testing.pages.LoginPage;
 import com.chromatech.utils.CucumberLogUtils;
 import com.chromatech.utils.WebDriverUtils;
@@ -11,7 +11,7 @@ import org.testng.Assert;
 
 public class Human_Resource_Module_Steps {
 
-    ExpenseModules expenseModules = new ExpenseModules();
+    DashboardPage dashboardPage = new DashboardPage();
     LoginPage loginPage = new LoginPage();
 
     @Given("a user is on the Chroma Tech Academy practice site {string}")
@@ -48,12 +48,23 @@ public class Human_Resource_Module_Steps {
 
     @When("the user clicks on the Human Resource module")
     public void the_user_clicks_on_the_human_resource_module() {
-        expenseModules.humanResourceModule.click();
+        dashboardPage.humanResourceModule.click();
         CucumberLogUtils.logScreenShot();
 
     }
     @Then("the following submodules are displayed {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}")
-    public void the_following_submodules_are_displayed(String staffDirectory, String staffAttendance, String payRoll, String aproveLeaveRequest, String applyLeave, String leaveType, String string7, String string8, String string9) {
+    public void the_following_submodules_are_displayed(String staffDirectory, String staffAttendance, String payRoll, String approveLeaveRequest, String applyLeave, String leaveType, String teachersRating, String department, String designation) {
+        Assert.assertTrue(dashboardPage.staffDirectorySubModule.isDisplayed());
+        Assert.assertTrue(dashboardPage.staffAttendanceSubModule.isDisplayed());
+        Assert.assertTrue(dashboardPage.payrollSubModule.isDisplayed());
+        Assert.assertTrue(dashboardPage.approveLeaveRequestSubModule.isDisplayed());
+        Assert.assertTrue(dashboardPage.applyLeaveSubModule.isDisplayed());
+        Assert.assertTrue(dashboardPage.leaveTypeSubModule.isDisplayed());
+        Assert.assertTrue(dashboardPage.teachersRatingSubModule.isDisplayed());
+        Assert.assertTrue(dashboardPage.departmentSubModule.isDisplayed());
+        Assert.assertTrue(dashboardPage.designationSubModule.isDisplayed());
+        CucumberLogUtils.logScreenShot();
+
 
     }
 }
