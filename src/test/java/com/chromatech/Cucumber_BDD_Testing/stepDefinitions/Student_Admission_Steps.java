183f4c55d5d9f0d94cff08e0dc5dc9b229246e93
Diskobots <chrisccain@gmail.com>
2024-07-18 15:21:37 -0400

    Updating my changes

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Student_Admission_Steps.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Student_Admission_Steps.java
@@ -1,4 +1,24 @@
 package com.chromatech.Cucumber_BDD_Testing.stepDefinitions;
 
+import com.chromatech.Cucumber_BDD_Testing.pages.BulkDeletePage;
+import com.chromatech.utils.CommonMethods;
+import io.cucumber.java.en.Then;
+
 public class Student_Admission_Steps {
+
+    BulkDeletePage bulkDeletePage = new BulkDeletePage();
+
+    @Then("test account is reset with admission number {string}, class {string}, section {string}")
+    public void test_account_is_reset_with_admission_number_class_section(String admissionNumber, String className, String sectionName) {
+        bulkDeletePage.bulkDeleteSubModule.click();
+        CommonMethods.selectDropDownValueByVisibleText(bulkDeletePage.classDropDown, className);
+        CommonMethods.selectDropDownValueByVisibleText(bulkDeletePage.sectionDropDown, sectionName);
+        bulkDeletePage.searchButton.click();
+        CommonMethods.waitForClickabilityOfElement(BulkDeletePage.dynamicDeleteRecordLocator(admissionNumber));
+        CommonMethods.scrollIntoView(BulkDeletePage.dynamicDeleteRecordLocator(admissionNumber));
+        BulkDeletePage.dynamicDeleteRecordLocator(admissionNumber).click();
+        CommonMethods.scrollIntoView(bulkDeletePage.deleteButton);
+        bulkDeletePage.deleteButton.click();
+        CommonMethods.acceptAlert();
+    }
 }
