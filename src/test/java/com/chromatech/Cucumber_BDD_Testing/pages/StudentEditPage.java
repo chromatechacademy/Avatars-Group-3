6e41bdc46ef884c3ac9f6a8541372a773e8fafe1
kdavey <keindavey@gmail.com>
2024-07-22 21:05:33 -0400

    Formatting

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/StudentEditPage.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/StudentEditPage.java
@@ -23,7 +23,7 @@ public class StudentEditPage {
     @FindBy(xpath = "//button[@type='submit'][normalize-space()='Save']")
     public WebElement saveButton;
 
-    /* Edit Button */
+    /* EDIT BUTTONad */
     @FindBy(xpath = "//a[contains(@href, 'student/edit')]")
     public WebElement editButton;
 }
