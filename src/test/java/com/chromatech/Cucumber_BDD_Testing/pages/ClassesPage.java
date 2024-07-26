7d68be408b0c8002bc6499fe645c46528e0dfaa7
Valerie_Ya <valerie_ya@Valerie-Yas-MacBook-Air.local>
2024-07-26 19:02:58 -0400

    Ability to add and delete classes

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/ClassesPage.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/pages/ClassesPage.java
@@ -1,11 +1,29 @@
 package com.chromatech.Cucumber_BDD_Testing.pages;
 
 import com.chromatech.utils.WebDriverUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class ClassesPage {
     public ClassesPage() {
         PageFactory.initElements(WebDriverUtils.driver, this);
     }
+
+    @FindBy(xpath = "//input[@id='class']")
+    public WebElement classNameTextBox;
+
+    @FindBy (xpath ="//input[@value='229']")
+    public WebElement manualTestindDropDownButton;
+
+    @FindBy (xpath = "//button[@type='submit'][normalize-space()='Save']")
+    public WebElement saveButton;
+
+    @FindBy (xpath ="//body[1]/div[1]/div[1]/section[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/table[1]/tbody[1]/tr[1]/td[3]/a[2]")
+    public WebElement deleteClassButton;
+
+
+
+
 
 }
--- src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Ability_To_Add_And_Delete_Classes_Steps.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Ability_To_Add_And_Delete_Classes_Steps.java
@@ -1,4 +1,49 @@
 package com.chromatech.Cucumber_BDD_Testing.stepDefinitions;
 
+import com.chromatech.Cucumber_BDD_Testing.pages.ClassesPage;
+import com.chromatech.Cucumber_BDD_Testing.pages.DashboardPage;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Ability_To_Add_And_Delete_Classes_Steps {
+    DashboardPage dashboardPage = new DashboardPage();
+    ClassesPage classesPage = new ClassesPage();
+
+    @When("clicks on the Academics module")
+    public void clicks_on_the_academics_module() {
+        dashboardPage.academicsModule.click();
+    }
+
+    @When("clicks on the Class submodule")
+    public void clicks_on_the_class_submodule() {
+        dashboardPage.classClassSubModule.click();
+
+    }
+
+    @When("adds a class {string}")
+    public void adds_a_class(String string) {
+
+
+    }
+
+    @When("chooses Manual Testing in the checkbox")
+    public void chooses_manual_testing_in_the_checkbox() {
+
+    }
+
+    @When("clicks Save button")
+    public void clicks_save_button() {
+
+    }
+
+    @When("clicks Delete button")
+    public void clicks_delete_button() {
+
+    }
+
+    @Then("the user clicks Confirm button to delete the class")
+    public void the_user_clicks_confirm_button_to_delete_the_class() {
+
+    }
+
 }
