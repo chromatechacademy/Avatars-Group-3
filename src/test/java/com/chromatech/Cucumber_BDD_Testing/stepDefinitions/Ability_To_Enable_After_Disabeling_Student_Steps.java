ba109b3781e09b24515a3bdd55fa339a5146ee19
Nahun92 <andrade_nahun@yahoo.com>
2024-07-26 23:08:37 -0400

    Removed Hard Waits

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Ability_To_Enable_After_Disabeling_Student_Steps.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/stepDefinitions/Ability_To_Enable_After_Disabeling_Student_Steps.java
@@ -11,26 +11,21 @@ public class Ability_To_Enable_After_Disabeling_Student_Steps {
 
     @When("clicks on Disable Student button")
     public void clicks_on_disable_student_button() {
-        CommonMethods.sleep(5000);
         disabledStudentPage.disableButton.click();
         CommonMethods.acceptAlert();
-        CommonMethods.sleep(1000);
         disabledStudentPage.reasonDropdown.click();
         CommonMethods.selectDropDownValue("Very Loud", disabledStudentPage.reasonDropdown);
-        CommonMethods.sleep(2000);
         disabledStudentPage.submitButton.click();
     }
 
     @When("a user enables the student record")
     public void a_user_enables_the_student_record() {
-        CommonMethods.sleep(2000);
         disabledStudentPage.enableButton.click();
         CommonMethods.acceptAlert();
     }
 
     @Then("the student record is successfully enabled")
     public void the_student_record_is_successfully_enabled() {
-        CommonMethods.sleep(2000);
         CommonMethods.assertTrue(disabledStudentPage.disableButton.isDisplayed());
     }
 }
