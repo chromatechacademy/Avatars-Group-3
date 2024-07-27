3db1347aed43b93640cf1b952c3e1f59ba87854f
iamdez99 <diegojuarezbusiness@gmail.com>
2024-07-26 21:23:48 -0400

    Added Page Initializer objects

--- src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/PageInitializer.java
+++ src/test/java/com/chromatech/Cucumber_BDD_Testing/appsCommon/PageInitializer.java
@@ -1,6 +1,6 @@
 package com.chromatech.Cucumber_BDD_Testing.appsCommon;
 
-import com.chromatech.Cucumber_BDD_Testing.pages.LoginPage;
+import com.chromatech.Cucumber_BDD_Testing.pages.*;
 
 public class PageInitializer {
 
@@ -9,6 +9,17 @@ public class PageInitializer {
      * PAGE OBJECTS
      */
 
+    public static StudentAdmissionPage studentAdmissionPage;
+    public static DashboardPage dashboardPage;
+    public static StudentDetailsPage studentDetailsPage;
+    public static StudentEditPage studentEditPage;
+    public static BulkDeletePage bulkDeletePage;
+
     public static void initializeAllPages() {
+        studentAdmissionPage = new StudentAdmissionPage();
+        dashboardPage = new DashboardPage();
+        studentDetailsPage = new StudentDetailsPage();
+        studentEditPage = new StudentEditPage();
+        bulkDeletePage = new BulkDeletePage();
     }
 }
